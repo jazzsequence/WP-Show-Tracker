17b23b022b2f7174ad9ef4794dd9f90098273984
Chris Reynolds <chris@jazzsequence.com>
2016-03-14 12:11:53 -0600

    adds a helper method to get all the shows this week for a particular viewer

--- includes/class-helpers.php
+++ includes/class-helpers.php
@@ -50,4 +50,34 @@ class WPST_Helpers {
 		return ( wp_show_tracker()->options->get_option( 'wpst_start_day' ) ) ? esc_attr( wp_show_tracker()->options->get_option( 'wpst_start_day' ) ) : 'sunday';
 	}
 
+	/**
+	 * Returns the show count for the current week for the given viewer.
+	 * @param  string $viewer The wpst_viewer term slug.
+	 * @return int            The total number of shows watched by this viewer this week.
+	 */
+	public function get_show_count_this_week_for( $viewer ) {
+		// Get the shows for this week.
+		$shows = get_posts( array(
+			'post_type'   => 'wpst_show',
+			'nopaging'    => true,
+			'wpst_viewer' => $viewer,
+			'meta_query'  => array(
+				array(
+					'key'     => 'wpst_show_date',
+					'value'   => array( strtotime( sprintf( 'last %s', $this->get_start_day() ) ), strtotime( 'today' ) ),
+					'compare' => 'BETWEEN',
+				),
+			),
+		) );
+
+		// Loop through this week's shows and tally up the total count.
+		$count = 0;
+		foreach ( $shows as $show ) {
+			if ( $show_count = get_post_meta( $show['ID'], 'wpst_show_count', true ) ) {
+				$count = $count + absint( $show_count );
+			}
+		}
+
+		return $count;
+	}
 }
