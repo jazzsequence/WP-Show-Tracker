1458a6b99f302e5f8085ade0e4bb1f149c93658b
Chris Reynolds <chris@jazzsequence.com>
2016-03-24 21:26:44 -0600

    there's a flash where the hidden viewer is still visible, so make it look intentional and use a fadeOut instead

--- assets/js/maxshows.js
+++ assets/js/maxshows.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function($) {
-	console.log(maxshows.hidden_viewers);
 	$.each(maxshows.hidden_viewers, function(index,viewer){
-		$('input[value='+viewer+']').parent().hide();
+		$('input[value='+viewer+']').parent().fadeOut();
 	});
 });
