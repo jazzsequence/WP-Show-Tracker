f754b2ab8025fdd29b8d2645c583e23b9400bc76
Chris Reynolds <chris@jazzsequence.com>
2016-03-28 10:49:13 -0600

    rename js file

---
+++ assets/js/show-tracker.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+	if ( showtracker.hidden_viewers ) {
+		$.each(showtracker.hidden_viewers, function(index,viewer){
+			$('input[value='+viewer+']').parent().fadeOut();
+		});
+	}
+
+	$('input#submitted_post_title').suggest(showtracker.admin_ajax + '?action=wpst_suggest');
+	console.log(showtracker.admin_ajax);
+	console.log($('input#submitted_post_title').suggest(showtracker.admin_ajax + '?action=wpst_suggest'));
+});
