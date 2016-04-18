f8ab6be7e2dc9c9cf418365d17c82fcab6b7f0c1
Chris Reynolds <chris@jazzsequence.com>
2016-04-18 10:55:05 -0600

    adds a method to render the output for the show count this week shortcode
    #5

--- includes/class-shortcodes.php
+++ includes/class-shortcodes.php
@@ -34,4 +34,19 @@ class WPST_Shortcodes {
 	 */
 	public function hooks() {
 	}
+
+	/**
+	 * Renders the output of the show count this week for viewer.
+	 * @param  object $viewer The viewer WP_Term object.
+	 * @param  string $slug   The viewer term slug.
+	 * @return string         The output to display.
+	 */
+	private function get_show_count_this_week_for_viewer( $viewer, $slug ) {
+		$show_count_this_week = wpst()->helpers->get_show_count_this_week_for( $viewer_slug );
+		$output = '<div class="alert alert-info"><p><span class="show-count" id="show-count-for-' . $viewer_slug . '">';
+		$output .= '<label for="' . $viewer_slug . '">' . esc_attr( $viewer->name ) . ':</label>' . absint( $show_count_this_week );
+		$output .= '</span></p></div>';
+
+		return $output;
+	}
 }
