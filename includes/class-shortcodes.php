54c0bcb464d9e3df2480dd34dd8bd65607ee5939
Chris Reynolds <chris@jazzsequence.com>
2016-04-18 09:37:54 -0600

    include a new class to hold all the shortcodes
    #5 #7

---
+++ includes/class-shortcodes.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * WP Show Tracker Shortcodes
+ * @version <%= version %>
+ * @package WP Show Tracker
+ */
+
+class WPST_Shortcodes {
+	/**
+	 * Parent plugin class
+	 *
+	 * @var   class
+	 * @since NEXT
+	 */
+	protected $plugin = null;
+
+	/**
+	 * Constructor
+	 *
+	 * @since  NEXT
+	 * @param  object $plugin Main plugin object.
+	 * @return void
+	 */
+	public function __construct( $plugin ) {
+		$this->plugin = $plugin;
+		$this->hooks();
+	}
+
+	/**
+	 * Initiate our hooks
+	 *
+	 * @since  NEXT
+	 * @return void
+	 */
+	public function hooks() {
+	}
+}
