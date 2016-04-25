8f496854d830ac9aa3bb9ebc6ffb7a7c61bac2ad
Chris Reynolds <chris@jazzsequence.com>
2016-04-25 08:43:48 -0600

    output the stats in a table
    #5

--- includes/class-shortcodes.php
+++ includes/class-shortcodes.php
@@ -48,18 +48,45 @@ class WPST_Shortcodes {
 			'from'   => 'week',
 		), $atts );
 
+		$show_count_message = '<div class="wpst-shows"><table class="wpst-shows-for-user-table">
+			<thead>
+				<th>' . __( 'Viewer', 'wp-show-tracker' ) . '</th>
+				<th>' . __( 'Count', 'wp-show-tracker' ) . '</th>
+			</thead>
+		<tbody>';
+
+		$has_viewer = true;
+
 		// Get the viewer, make sure it's valid.
 		$viewer_slug = sanitize_title( $atts['viewer'] );
 		if ( 'all' !== $viewer_slug && $viewer = get_term_by( 'slug', $viewer_slug, 'wpst_viewer' ) ) {
-			$show_count_message = $this->the_show_count_for_viewer( $viewer, $viewer_slug, $atts['from'] );
+			$show_count_message .= $this->the_show_count_for_viewer( $viewer, $viewer_slug, $atts['from'] );
 		} elseif ( 'all' == $viewer_slug ) {
 			$viewers = get_terms( 'wpst_viewer', array( 'hide_empty' => false ) );
 			foreach ( $viewers as $viewer ) {
-				$show_count_message .= $show_count_message = $this->the_show_count_for_viewer( $viewer, $viewer_slug, $atts['from'] );
+				$show_count_message .= $this->the_show_count_for_viewer( $viewer, $viewer_slug, $atts['from'] );
 			}
 		} else {
-			$show_count_message = __( 'No valid viewer to display.', 'wp-show-tracker' );
+			$show_count_message .= '<tr colspan=2><td>' . __( 'No valid viewer to display.', 'wp-show-tracker' ) . '</td></tr>';
+			$has_viewer = false;
 		}
+
+		$show_count_message .= '</tbody></table>';
+
+		// Only display the show count message if there was a viewer.
+		if ( $has_viewer ) {
+			$show_count_message .= '<div class="alignright"><em>';
+			if ( 'alltime' == $atts['from'] ) {
+				$show_count_message .= __( 'Total shows watched', 'wp-show-tracker' );
+			} else {
+				$start_day = ( 'week' == $atts['from'] ) ? strtotime( sprintf( 'last %s', wpst()->helpers->get_start_day() ) ) : strtotime( $atts['from'] );
+				$start     = ( strtotime( 'today' ) == strtotime( $start_day ) ) ? strtotime( 'today midnight' ) : $start_day;
+				$show_count_message .= sprintf( __( 'Shows watched since %s', 'wp-show-tracker' ), date( get_option( 'date_format' ), $start ) );
+			}
+			$show_count_message .= '</em></div>';
+		}
+
+		$show_count_message .= '</div>';
 
 		$output = $show_count_message;
 
@@ -78,9 +105,10 @@ class WPST_Shortcodes {
 	 */
 	private function the_show_count_for_viewer( $viewer, $slug, $from ) {
 		$show_count = wpst()->helpers->get_show_count_for( $slug, $from );
-		$output = '<div class="alert alert-info"><p><span class="show-count" id="show-count-for-' . $slug . '">';
-		$output .= '<label for="' . $slug . '">' . esc_attr( $viewer->name ) . ':</label> ' . absint( $show_count );
-		$output .= '</span></p></div>';
+		$output = '<tr class="show-count" id="show-count-for-' . $slug . '">';
+		$output .= '<td>' . esc_attr( $viewer->name ) . '</td> ';
+		$output .= '<td>' . absint( $show_count ) . '</td>';
+		$output .= '</tr>';
 
 		return $output;
 	}
