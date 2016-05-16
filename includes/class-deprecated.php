9531995d0563f66841e24542c3c2e65fd624d495
Chris Reynolds <chris@jazzsequence.com>
2016-05-16 13:50:46 -0600

    add query arg to run a migration process from old beta versions

--- includes/class-deprecated.php
+++ includes/class-deprecated.php
@@ -39,5 +39,37 @@ class WPST_Deprecated {
 	 * @return void
 	 */
 	public function hooks() {
+		add_action( 'init', array( $this, 'migrate_from_old_version' ) );
+	}
+
+	public function migrate_from_old_version() {
+		if ( isset( $_GET ) && ! isset( $_GET['update_show_counts'] ) ) {
+			return;
+		}
+		delete_option( 'wpst_migrated_from_old_version' );
+		if ( $already_run = get_option( 'wpst_migrated_from_old_version' ) ) {
+			wp_die( esc_html__( 'Migration has already been run. Skipping migration.', 'wp-show-tracker' ), esc_html__( 'Show Tracker migration error', 'wp-show-tracker' ) );
+		}
+
+		// Get all the unique shows.
+		$shows = wpst()->helpers->get_show_list();
+
+		echo '<h3>' . esc_html_e( 'Shows updated', 'wp-show-tracker' ) . '</h3>';
+
+		// Each show probably has multiple different shows of that same title. Process each show individually.
+		foreach ( $shows as $show_group ) {
+			// Skip shows with no title.
+			if ( '' == $show_group ) {
+				continue;
+			}
+
+			$this->process_show( $this->get_all_instances_of_show( $show_group ) );
+		}
+
+		// Make sure we don't run this again.
+		// add_option( 'wpst_migrated_from_old_version', true );
+
+		wp_die();
+	}
 	}
 }
